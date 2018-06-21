3bf1fda98fe4160d96a1515d553207b0f0dd0959
Doai Tran <josdoaitran@gmail.com>
2018-06-21 07:29:50 +0700

    Update Basic.sh

--- Basic.sh
+++ Basic.sh
@@ -1,6 +1,29 @@
 # .sh
 
+# command in directory
+# List all items on directory
+
 ll
+
 ls
 
 ls -ltr
+
+# Copy a file / folder
+
+cp copied-file.txt new-file.txt
+
+
+# Move a file / folder => Rename file / folder.
+
+mv file.ts new.ts
+
+# Delete a file
+
+rm deleted-file.ts
+
+# Delete a folder
+
+rm -rfv deleted-folder
+
+
